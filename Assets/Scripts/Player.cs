d9b847e62b79f9af6b764857fdff19dedc13aa4a
irisk2050 <irisk2050@gmail.com>
2022-03-01 19:26:38 -0800

    made DarkEnemy animations and Player animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Movement_variables
+    public float speed;
+    [SerializeField]
+    Animator anim;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
