e95776d466ac1c395d55c93748c930066412c9c4
vivianrdu <vivian_du@berkeley.edu>
2022-03-06 22:21:35 -0800

    add player animations and edit controller

--- Assets/Scripts/Nightlight.cs
+++ Assets/Scripts/Nightlight.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine;
 
 public class Nightlight : MonoBehaviour
@@ -9,54 +10,76 @@ public class Nightlight : MonoBehaviour
     #endregion
 
     #region Physics_components
-    BoxCollider2D nightlightBC;
+    BoxCollider2D nightlightTBC;
     public float radius;
+    private bool timerIsRunning;
+    Light2D fire;
     #endregion
 
     #region Targeting_variables
     public Transform player;
+    public Transform checkpoint;
     #endregion
 
 
     // Start is called before the first frame update
     void Start()
     {
-        nightlightBC = GetComponent<BoxCollider2D>();
+        nightlightTBC = GetComponent<BoxCollider2D>();
+        fire = GetComponentInChildren<Light2D>();
         anim = GetComponent<Animator>();
+        timerIsRunning = false;
     }
 
-    private void Update()
+    void Update()
     {
+        float distFromPlayer = Vector2.Distance(player.position, transform.position);
+
         if (player == null)
         {
             return;
         }
         else
         {
-            TurnOn();
+            if ((Input.GetKeyDown(KeyCode.E)) && (distFromPlayer <= radius) && (timerIsRunning == false))
+            {
+                Debug.Log("Picked up");
+                PickedUp();
+                StartCoroutine(Countdown());
+            }
         }
     }
 
-    public void TurnOn()
+    private void PickedUp()
     {
-        Debug.Log("timer is " + anim.GetFloat("timer").ToString());
+        fire.intensity = 1;
+        timerIsRunning = true;
+    }
 
-        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, radius, Vector2.zero);
-        foreach (RaycastHit2D hit in hits)
+    private void Reset()
+    {
+        StopCoroutine(Countdown());
+        StartCoroutine(Countdown());
+    }
+
+    private void TurnOff()
+    {
+        timerIsRunning = false;
+    }
+
+    public IEnumerator Countdown()
+    {
+        float totalTransitionTime = 20f;
+        float elapsedTime = 0;
+
+        while (fire.intensity >= 0.1)
         {
-            if (anim.GetFloat("timer") > 0)
-            {
-                Debug.Log("Player detected.");
-                GetComponent<Animator>().SetBool("on", true);
-                anim.SetFloat("timer", anim.GetFloat("timer") - Time.deltaTime);
-                Debug.Log("timer at " + anim.GetFloat("timer").ToString());
-            }
-            else if (anim.GetFloat("timer") <= 0)
-            {
-                GetComponent<Animator>().SetBool("on", false);
-                anim.SetFloat("timer", -1);
-                Debug.Log("timer at " + anim.GetFloat("timer").ToString());
-            }
+            fire.intensity = Mathf.Lerp(fire.intensity, 0, elapsedTime / totalTransitionTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
+
+        TurnOff();
+        Debug.Log("Turned off");
     }
 }
