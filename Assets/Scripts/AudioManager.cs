9be5f7109de88399623d5e38ccc6aea4f4135f9c
irisk2050 <irisk2050@gmail.com>
2022-03-22 15:21:09 -0700

    added background music to StartMenu scene with audio fade in

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
@@ -14,6 +15,9 @@ public class AudioManager : MonoBehaviour
     public AudioMixerGroup musicGroup;
     public AudioMixerGroup sfxGroup;
     public AudioMixerGroup foleyGroup;
+
+    private static bool keepFadingIn;
+    private static bool keepFadingOut;
 
     // Use this for initialization
     void Awake()
@@ -27,8 +31,8 @@ public class AudioManager : MonoBehaviour
             if (sound.group == "Music")
             {
                 sound.source.outputAudioMixerGroup = musicGroup;
-                sound.source.loop = true;
                 sound.source.playOnAwake = true;
+                sound.source.loop = true;
             }
             else if (sound.group == "Foley")
             {
@@ -49,7 +53,42 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-        Play("Music");
+        if (SceneManager.GetActiveScene().name == "0.StartMenu")
+        {
+            PlayMusic("StartMenuBGMusic");
+        }
+        else if (SceneManager.GetActiveScene().name == "0.Tutorial")
+        {
+
+        }
+        else if (SceneManager.GetActiveScene().name == "1.DarkScene")
+        {
+
+        }
+        else if (SceneManager.GetActiveScene().name == "1.DarkSceneAnimated")
+        {
+
+        }
+        else if (SceneManager.GetActiveScene().name == "2.WaterScene")
+        {
+
+        }
+        else if (SceneManager.GetActiveScene().name == "2.WaterScene")
+        {
+
+        }
+        else if (SceneManager.GetActiveScene().name == "2.WaterSceneAnimated")
+        {
+
+        }
+        else if (SceneManager.GetActiveScene().name == "3.PeopleScene")
+        {
+
+        }
+        else if (SceneManager.GetActiveScene().name == "3.PeopleSceneAnimated")
+        {
+
+        }
     }
 
     public void Play(string name)
@@ -60,7 +99,35 @@ public class AudioManager : MonoBehaviour
             Debug.Log("No music");
             return;
         }
-        s.source.Play();
+        s.source.PlayOneShot(s.source.clip);
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("No music");
+            return;
+        }
+        s.source.volume = 0;
+        StartCoroutine(FadeTrackIn(s.source));
+    }
+
+    private IEnumerator FadeTrackIn(AudioSource newSource)
+    {
+        float timeToFade = 1.25f;
+        float timeElapsed = 0;
+
+        newSource.PlayOneShot(newSource.clip);
+
+        while (timeElapsed < timeToFade)
+        {
+            newSource.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+            timeElapsed += Time.deltaTime;
+            Debug.Log(newSource.volume.ToString());
+            yield return null;
+        }
     }
 
     public bool IsPlaying(string audio)
@@ -86,4 +153,5 @@ public class AudioManager : MonoBehaviour
             s.source.Stop();
         }
     }
+
 }
